5d7e2bd0edbd2b0c72af25bc3e2b394a931f8dbf
emmanuel-nwafor <167652757+emmanuel-nwafor@users.noreply.github.com>
2024-08-29 19:07:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// alert("This site is under construction...")
+function contact() {
+    alert("This site is under construction");
+}
+function faq() {
+    alert("This site is under construction");
+}
+function email() {
+    alert("This site is under construction");
+}
+function center() {
+    alert("This site is under construction");
+}
